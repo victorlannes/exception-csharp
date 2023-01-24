85f350dc37844939ecf4745f361097676d531b1b
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-01-24 19:22:06 -0300

    Adicionar arquivos de projeto.

---
+++ exception/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace exception.Entities.Exceptions {
+     class DomainException : ApplicationException
+     {
+        public DomainException(string message) : base(message) 
+        {
+
+        }
+     }
+}
---
+++ exception/Entities/Reservation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using exception.Entities.Exceptions;
+namespace exception.Entities {
+     class Reservation {
+
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut) {
+
+            if (checkOut <= checkIn) {
+                throw new DomainException("Error in reservation: Check-out date must be");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration() {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now) {
+                throw new DomainException( "Error in reservation: Reservation dates for update must be future dates!");
+            }
+            if (checkOut <= checkIn) {
+                throw new DomainException ("Error in reservation: Check-out date must be");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+           
+        }
+
+        public override string ToString() {
+            return "Room " + RoomNumber + ", check-in: " + CheckIn.ToString("dd/MM/yyyy") + CheckOut.ToString("dd/MM/yyyy") + ", " + Duration() + " nights";
+        }
+    }
+}
